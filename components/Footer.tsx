24cd453b31cd555bfbac398a0f4158be2d2b2195
Pradhyum Arora <official.pradhyum@gmail.com>
2024-03-01 12:22:44 +0530

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="text-center bg-gray-100 py-3">
+      Empower Creativity, Amplify Wealth. 🔥
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between px-8 py-4">
+      <Image src="/logo.png" alt="logo" width={50} height={50} />
+      <Link href="/login">Login / Signup</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
