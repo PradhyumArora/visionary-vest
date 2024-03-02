26ef4f231a98231abcb63e9248f167109308104d
Sneha Rajput <sneharjpt27@gmail.com>
2024-03-02 10:37:42 +0530

    explore page

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/explore"],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ["/no-auth-in-this-route"],
